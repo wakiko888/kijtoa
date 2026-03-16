013bb76dde5b746b6c5addc4f71ddeab535e1dae
Zzmarog <zzmarog@gmail.com>
2026-03-16 20:13:36 +0100

    V 0.1

--- kijtoa.py
+++ kijtoa.py
@@ -1,2 +1,18 @@
-print(caca)
-int(input("Combien de boule a tu ?"))
+import random
+import time 
+#Import on top 
+
+
+def attack(damage, health):
+    health += -damage
+    return health
+
+Hero_Hp = 100
+Golbin_attack = random.randint(1, 15)
+Hero_Hp = attack(Golbin_attack, Hero_Hp)
+if Golbin_attack == 1:
+    print("Le Goblin attaque pour 1 miserable dégât")
+else:
+    print("Le Golblin attque pour ", Golbin_attack, "dégâts")
+
+print(Hero_Hp)
